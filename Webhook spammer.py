d8511b2d0cdebbe1e14c2fe61883b81cf780eb09
Hex | 0x1F608 <143428679+0x1F608@users.noreply.github.com>
2023-09-19 22:19:36 +0100

    Update Webhook spammer.py
    Added small fixes

--- Webhook spammer.py
+++ Webhook spammer.py
@@ -5,23 +5,41 @@ import threading
 from colorama import Fore
 from pystyle import *
 
+if os.name == "nt":
+    os.system("title Made with love by 0x1F608")
+else:
+    pass
+
 def clear():
     if os.name == "nt":
         os.system("cls")
     else:
         os.system("clear")
 
-clear()
-print("-------------------------------------------------------")
-print(Colorate.Horizontal(Colors.blue_to_cyan, "       0x1F608 super awesome webhook spammer!!"))
-print("-------------------------------------------------------\n")
+madeby = "Made by https://github.com/0x1F608"
 
-webhook = input("Webhook: ")
-webhookname = input("Webhookname: ")
-sleep = input("Delay: ")
-count = input("Request number: ")
-message = input("Message: ")
-threadingoption = input("Threading? [y/n]: ")
+clear()
+#print("-------------------------------------------------------")
+#print(Colorate.Horizontal(Colors.blue_to_cyan, "       0x1F608 super awesome webhook spammer!!"))
+#print("-------------------------------------------------------\n")
+print(Colorate.Horizontal(Colors.blue_to_cyan, f"""   ____      ______________ ____  ____                                                   
+  / __ \_  _<  / ____/ ___// __ \( __ )   _________  ____ _____ ___  ____ ___  ___  _____
+ / / / / |/_/ / /_  / __ \/ / / / __  |  / ___/ __ \/ __ `/ __ `__ \/ __ `__ \/ _ \/ ___/
+/ /_/ />  </ / __/ / /_/ / /_/ / /_/ /  (__  ) /_/ / /_/ / / / / / / / / / / /  __/ /    
+\____/_/|_/_/_/    \____/\____/\____/  /____/ .___/\__,_/_/ /_/ /_/_/ /_/ /_/\___/_/     
+                                           /_/   
+{madeby}\n                                  
+"""))
+
+
+
+
+webhook = input(f"{Fore.LIGHTBLUE_EX}Webhook:{Fore.RESET} ")
+webhookname = input(f"{Fore.LIGHTBLUE_EX}Webhookname:{Fore.RESET} ")
+sleep = input(f"{Fore.LIGHTBLUE_EX}Delay:{Fore.RESET} ")
+count = input(f"{Fore.LIGHTBLUE_EX}Request number:{Fore.RESET} ")
+message = input(f"{Fore.LIGHTBLUE_EX}Message:{Fore.RESET} ")
+threadingoption = input(f"{Fore.LIGHTBLUE_EX}Threading? [y/n]:{Fore.RESET} ")
 
 if threadingoption == "y":
     threadingoption = "True"
@@ -35,24 +53,34 @@ data = {
 count = int(count)
 sleep = int(sleep)
 
-amountcount = 0
+
 
 Write.Print("\nStarted spamming webhook!\n", Colors.blue_to_cyan)
+print("")
 
-print(threadingoption)
+goodreqs = 0
+failedreqs = 0
+
+start_time = time.time()
+
 def spamwebhook():
+    global goodreqs, failedreqs
+    
     currenttime = time.strftime("%H:%M", time.localtime())  # Get current time here
     time.sleep(sleep)
     response = requests.post(webhook, data=data)
     if response.status_code == 200:
-        print(f"{Fore.GREEN}[+]{Fore.RESET} [{currenttime}] | Successful requests sent!")  #| [{amountcount}/{count}]
+        print(f"{Fore.GREEN}[+]{Fore.RESET} [{currenttime}] | Successful request sent!")
+        goodreqs += 1
     else:
-        print(f"{Fore.RED}[+]{Fore.RESET} [{currenttime}] Failed to send webhook requests {response.status_code}")  #| [{amountcount}/{count}]
+        print(f"{Fore.RED}[-]{Fore.RESET} [{currenttime}] Failed to send webhook {response.status_code}")
+        failedreqs += 1
 
 if threadingoption == "True":
     threads = []
 
     for i in range(count):
+        
         t = threading.Thread(target=spamwebhook)
         t.daemon = True
         threads.append(t)
@@ -66,3 +94,9 @@ if threadingoption == "True":
 elif threadingoption == "False":
     for i in range(count):
         spamwebhook()
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+
+Write.Print(f"\nFinished spamming webhook!\n", Colors.blue_to_cyan)
+print(f"{Fore.LIGHTBLUE_EX}Successful requests:{Fore.RESET} {goodreqs}\n{Fore.LIGHTBLUE_EX}Failed requests:{Fore.RESET} {failedreqs}\n{Fore.LIGHTBLUE_EX}Elapsed time:{Fore.RESET} {elapsed_time:.2f} seconds\n")
